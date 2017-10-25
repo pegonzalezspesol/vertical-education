ffbda4c0b97da3e56fc93b1bf3917a159225c2b1
Angel Moya Pardo <amoyapardo@gmail.com>
2017-10-25 18:42:55 +0200

    WIP

--- education_evaluation/models/exam.py
+++ education_evaluation/models/exam.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from openerp import models, api, fields, _
+from odoo import models, api, fields, _
 
 
 class EducationExam(models.Model):
